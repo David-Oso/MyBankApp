f3968954597be5364fd7c3f3b17d839c1cdcaecf
David <osodavid001@gmail.com>
2023-12-29 16:41:30 +0100

    authentication provider

--- MyBankApp/src/main/java/com/bank/MyBankApp/appSecurity/MyBankAuthenticationManager.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/appSecurity/MyBankAuthenticationManager.java
@@ -1,4 +1,19 @@
 package com.bank.MyBankApp.appSecurity;
 
-public class MyBankAuthenticationManager {
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class MyBankAuthenticationManager implements AuthenticationManager {
+    private final AuthenticationProvider authenticationProvider;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return authenticationProvider.authenticate(authentication);
+    }
 }
