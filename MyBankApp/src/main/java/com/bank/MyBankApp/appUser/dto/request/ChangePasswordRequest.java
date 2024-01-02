77198072a2b981c9385c2c47431e0d8cabd88a3a
David <osodavid001@gmail.com>
2024-01-02 14:00:10 +0100

    annotated change passowrd reqeust

--- MyBankApp/src/main/java/com/bank/MyBankApp/appUser/dto/request/ChangePasswordRequest.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/appUser/dto/request/ChangePasswordRequest.java
@@ -1,16 +1,35 @@
 package com.bank.MyBankApp.appUser.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import static com.bank.MyBankApp.utilities.ValidationUtils.PASSWORD_REGEX;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class ChangePasswordRequest {
+    @NotEmpty(message = "field current password cannot be empty")
+    @NotBlank(message = "field current password cannot be blank")
+    @Pattern(regexp = PASSWORD_REGEX, message = "Password must " +
+            "contain at least one capital letter, one small letter, a number and special character(@$!%*?&)")
     private String currentPassword;
+
+    @NotEmpty(message = "field new password cannot be empty")
+    @NotBlank(message = "field new password cannot be blank")
+    @Pattern(regexp = PASSWORD_REGEX, message = "Password must " +
+            "contain at least one capital letter, one small letter, a number and special character(@$!%*?&)")
     private String newPassword;
+
+    @NotEmpty(message = "field confirm password cannot be empty")
+    @NotBlank(message = "field confirm password cannot be blank")
+    @Pattern(regexp = PASSWORD_REGEX, message = "Password must " +
+            "contain at least one capital letter, one small letter, a number and special character(@$!%*?&)")
     private String confirmPassword;
 }
--- MyBankApp/src/main/java/com/bank/MyBankApp/utilities/ValidationUtils.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/utilities/ValidationUtils.java
@@ -2,7 +2,7 @@ package com.bank.MyBankApp.utilities;
 
 public final class ValidationUtils {
 public static final String EMAIL_REGEX_STRING = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-    public static final String PASSWORD_REGEX_STRING = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
     public static final String NAME_REGEX = "^[A-Z][a-zA-Z]{0,39}$";
     public static final String PHONE_NUMBER_REGEX = "^(\\+?234|0)[789][01]\\d{8}$";
     public static final String PIN_REGEX = "^\\d{4}$";
