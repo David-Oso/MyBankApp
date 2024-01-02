ecfba15837492c837ba664b9352cd7ba7e561b0e
David <osodavid001@gmail.com>
2024-01-02 13:54:35 +0100

    annotated transfer request

--- MyBankApp/src/main/java/com/bank/MyBankApp/account/dto/request/TransferRequest.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/account/dto/request/TransferRequest.java
@@ -1,5 +1,6 @@
 package com.bank.MyBankApp.account.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,29 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+import static com.bank.MyBankApp.utilities.ValidationUtils.PIN_REGEX;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class TransferRequest {
+    @NotNull(message = "field account id cannot be null")
     private Integer accountId;
+
+    @NotNull(message = "field recipient iban cannot be null")
+    @NotBlank(message = "field recipient iban cannot be blank")
+    @NotEmpty(message = "field recipient iban cannot be empty")
     private String recipientIban;
+
+    @NotNull(message = "field amount cannot be null")
+    @DecimalMin(value = "1.00", message = "Amount must be greater than or equal to ₦1.00")
+    @DecimalMax(value = "2000000.00", message = "Amount must be lesser than or equal to ₦2,000,000.00")
     private BigDecimal amount;
+
+    @NotNull(message = "field pin cannot be null")
+    @NotBlank(message = "field pin cannot be blank")
+    @NotEmpty(message = "field pin cannot be empty")
+    @Pattern(regexp = PIN_REGEX, message = "Enter your 4 digits pin")
     private String pin;
 }
