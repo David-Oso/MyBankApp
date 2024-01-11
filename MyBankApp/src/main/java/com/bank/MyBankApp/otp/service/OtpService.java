9a6de21104c70c37426e2ff976bb7b404956d421
David <osodavid001@gmail.com>
2024-01-11 08:57:22 +0100

    otp service

--- MyBankApp/src/main/java/com/bank/MyBankApp/otp/service/OtpService.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/otp/service/OtpService.java
@@ -1,4 +1,10 @@
 package com.bank.MyBankApp.otp.service;
 
+import com.bank.MyBankApp.appUser.model.AppUser;
+import com.bank.MyBankApp.otp.OtpEntity;
+
 public interface OtpService {
+    String generateAndSaveOtp(AppUser appUser);
+    OtpEntity validateOtp(String otp, Integer appUserId);
+    void deleteOtp(OtpEntity otpEntity);
 }
