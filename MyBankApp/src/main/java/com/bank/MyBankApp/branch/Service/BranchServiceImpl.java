d9fd090e29b608624a40c756994e834fd3fba572
David <osodavid001@gmail.com>
2024-01-25 21:57:54 +0100

    branch service implementation

--- MyBankApp/src/main/java/com/bank/MyBankApp/branch/Service/BranchServiceImpl.java
+++ MyBankApp/src/main/java/com/bank/MyBankApp/branch/Service/BranchServiceImpl.java
@@ -3,6 +3,7 @@ package com.bank.MyBankApp.branch.Service;
 import com.bank.MyBankApp.account.model.Account;
 import com.bank.MyBankApp.address.model.Address;
 import com.bank.MyBankApp.appUser.model.AppUser;
+import com.bank.MyBankApp.appUser.model.Role;
 import com.bank.MyBankApp.branch.dto.request.CreateBranchRequest;
 import com.bank.MyBankApp.branch.dto.response.BranchResponse;
 import com.bank.MyBankApp.branch.dto.response.CreateBranchResponse;
@@ -11,6 +12,7 @@ import com.bank.MyBankApp.branch.repository.BranchRepository;
 import com.bank.MyBankApp.exception.NotFoundException;
 import com.bank.MyBankApp.loan.model.Loan;
 import com.bank.MyBankApp.mail.MailService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,6 +27,49 @@ public class BranchServiceImpl implements BranchService{
     private final MailService mailService;
     @Value(("${spring.mail.username}"))
     private String senderEmail;
+    @Value("${branch.number}")
+    private String branchNumber;
+    @Value("${bank.phone_number}")
+    private String branchPhoneNumber;
+    @Value("${branch.email}")
+    private String branchEmail;
+    @Value("${branch.password}")
+    private String branchPassword;
+
+
+    @PostConstruct
+    private void createBranch(){
+        if(branchRepository.count() == 0){
+            Branch branch = new Branch();
+            branch.setAppUser(createAppUser());
+            branch.setBranchAddress(createAddress());
+            branch.setBranchNumber(branchNumber);
+        }
+    }
+
+    private AppUser createAppUser() {
+        AppUser appUser = new AppUser();
+        appUser.setFirstName("Branch1");
+        appUser.setLastName("Branch1");
+        appUser.setEmail(branchEmail);
+        appUser.setPhoneNumber(branchPhoneNumber);
+        appUser.setPassword(branchPassword);
+        appUser.setRole(Role.BRANCH_ADMIN);
+        appUser.setEnabled(true);
+        return appUser;
+    }
+
+
+    private Address createAddress() {
+        Address address = new Address();
+        address.setStreetNumber(12);
+        address.setStreetName("Odanikin");
+        address.setTownName("Shagari");
+        address.setCityName("Akure");
+        address.setState("Ondo");
+        address.setCountry("Nigeria");;
+        return address;
+    }
 
     @Override
     public CreateBranchResponse createNewBranch(CreateBranchRequest request) {
